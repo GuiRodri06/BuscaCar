9ef182a572deceed3598763f9391cfaf73eab20d
Gigi Lucas <gigi16lucas@gmail.com>
2025-03-24 18:23:12 +0000

    criar marcas.java e atributos e set/get

---
+++ src/model/entities/carros/marcas.java
@@ -0,0 +1,22 @@
+package model.entities.carros;
+
+public class marcas {
+
+    private String modelo;
+
+    
+
+    public marcas(String model) {
+        this.modelo = model;
+    }
+
+    public String getName() {
+        return modelo;
+    }
+
+    public void setName(String name) {
+        this.modelo = name;
+    }
+
+    
+}
